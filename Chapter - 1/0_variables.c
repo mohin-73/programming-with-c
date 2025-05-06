3aae36c9afa0b110d0beccd4d8da9665e017ce52
Eklas Mohin <mohin73k@gmail.com>
2025-05-06 07:05:30 +0000

    Variable Declaration & Initialization

--- Chapter - 1/0_variables.c
+++ Chapter - 1/0_variables.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    int a; // Variable declaration
-    a = 7; // Variable initialization
+    int a; // Variable Declaration
+    a = 7; // Variable Initialization
     printf("%d\n", a); // Print the value of 'a'
     return 0;
 }
