095fc1f582c343ddb250aae0978bcbe99692ccb7
Eklas Mohin <mohin73k@gmail.com>
2025-05-06 13:52:40 +0000

    Variable Declaration & Initialization

---
+++ Chapter - 1/variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int age; // Declaration
+    age = 19; // Initialization
+    printf("%d\n", age); // Print stored data/value
+
+    float gpa = 3.9; // Declaration & Initialization
+    printf("%f\n", gpa); // Print stored data/value
+
+    return 0;
+}
